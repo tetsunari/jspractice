204360618d758ce726bbba717b7108856355a03f
tetsunari <mtst.tty556@gmail.com>
2021-02-18 19:13:31 +0900

    first commit

---
+++ javascript/js/main.js
@@ -0,0 +1,4 @@
+'use strict';
+
+console.log('Hello world from main.js!');
+// Console.log('Hello world from main.js!');    //JavaScript の命令では大文字と小文字が厳密に区別されるので、こちらを大文字の C としてしまうと、そういった命令はないのでエラーになる
