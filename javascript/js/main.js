9335ae51dd6f5a9a4a6c97490aa72d92ef5e1038
tetsunari <mtst.tty556@gmail.com>
2021-02-19 12:16:05 +0900

    アロー関数

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -1,29 +1,19 @@
-// 'use strict';
-
-// function sum(a, b, c) {
-//   // console.log(a + b + c);
-//   return a + b + c;
-// }
-
-// // sum(1, 2, 3);
-// // sum(3, 4, 5);
-
-// const total = sum(1, 2, 3) + sum(3, 4, 5); // 18
-// // console.log(total);
-
-/*------------------------------------------------------------------*/
-
 'use strict';
 
-// function sum(a, b, c) {
+// const sum = function(a, b, c) {
 //   return a + b + c;
-// }
+// };
+// const sum = (a, b, c) => a + b + c;  //上と同じ
 
-const sum = function(a, b, c) {
-  return a + b + c;
-};  //関数宣言には}の後に「;」を忘れないようにする
 
-const total = sum(1, 2, 3) + sum(3, 4, 5);
-console.log(total);
+// const total = sum(1, 2, 3) + sum(3, 4, 5);
+// console.log(total);
 
-//上と同じ結果になる
+
+
+
+// const double = function(a) {
+//   return a * 2;
+// };
+const double = a => a * 2;
+console.log(double(12));    //上と同じ
