abe650f157d6c49912092e9576738578e0621116
tetsunari <mtst.tty556@gmail.com>
2021-02-19 12:39:38 +0900

    querySelectorAll()

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -2,10 +2,21 @@
 
 {
   function update() {
-    // document.querySelector('h1').textContent = 'Changed!';
-    // document.querySelector('#target').textContent = 'Changed!';
-    document.getElementById('target').textContent = 'Changed!';     //上三つは同じ結果
+    // document.getElementById('target').textContent = 'Changed!';
+    // document.querySelector('p').textContent = 'Changed!';    //1番目のpタグ変わる
+    // document.querySelectorAll('p')[1].textContent = 'Changed!';  //２番目のpタグ変わる
+    document.querySelectorAll('p').forEach((p, index) => {
+      p.textContent = `${index}番目のpです！`;
+    });     //列ごとのpタグに作用する
   }
 
   setTimeout(update, 1000);
 }
+
+/** 
+ * 重要
+ * DOM で要素を取得する方法はこのようにいろいろありますが、 
+ * id 属性が付いていたら getElementById() 、 
+ * id 属性がなければ querySelector() か querySelectorAll() 
+ * を使ってあげると良いでしょう。
+ */
