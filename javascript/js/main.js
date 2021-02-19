7b1d1a7cbb8e1277e96e285e4931dbbf184ca361
tetsunari <mtst.tty556@gmail.com>
2021-02-19 11:59:21 +0900

    continue, break

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -1,15 +1,12 @@
 'use strict';
 
-// let hp = 100;
-
-// while (hp > 0) {
-//   console.log(`${hp} HP left!`);
-//   hp -= 15;
-// }
-
-let hp = -50;
-
-do {
-  console.log(`${hp} HP left!`);
-  hp -= 15;
-} while (hp > 0);   //一回は実行される
+for (let i = 1; i <= 10; i++) {
+  // if (i === 4) {
+  // if (i % 3 === 0) {
+  //   continue;    //ループをスキップできる
+  // }
+  if (i === 4) {
+    break;  //4の時終わる
+  }
+  console.log(i);
+}
