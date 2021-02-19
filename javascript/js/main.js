bbddc1f5b5c4168e4bcba975169b211143ae01e9
tetsunari <mtst.tty556@gmail.com>
2021-02-19 13:27:32 +0900

    classNameを使ってクラス属性を操作する方法

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -4,9 +4,7 @@
   document.querySelector('button').addEventListener('click', () => {
     const targetNode = document.getElementById('target');
 
-    targetNode.textContent = 'Changed!';
-    targetNode.title = 'This is title!';
-    targetNode.style.color = 'red';
-    targetNode.style.backgroundColor = 'skyblue';
+    // targetNode.className = 'my-color';   //クリックするとボーダーが消える
+    targetNode.className = 'my-color my-border';
   });
 }
