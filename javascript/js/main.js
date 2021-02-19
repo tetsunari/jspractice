a8ff79c0f8fd56724a4cc53744456fc5e0a8663a
tetsunari <mtst.tty556@gmail.com>
2021-02-19 12:32:00 +0900

    querySelector()やgetElementById()を使って要素を取得する方法

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -1,6 +1,11 @@
 'use strict';
 
 {
-  const x = 100;
-  console.log(x);
+  function update() {
+    // document.querySelector('h1').textContent = 'Changed!';
+    // document.querySelector('#target').textContent = 'Changed!';
+    document.getElementById('target').textContent = 'Changed!';     //上三つは同じ結果
+  }
+
+  setTimeout(update, 1000);
 }
