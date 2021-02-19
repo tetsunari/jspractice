25cf6a4c17dec84345bcd677cbb7214eb832b39d
tetsunari <mtst.tty556@gmail.com>
2021-02-19 11:09:32 +0900

    変数 let

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -1,13 +1,13 @@
 'use strict';
 
-'use strict';
+// 定数 const
+// 変数 let var
 
-// 定数
+let price = 150;    // varでも良い
+const prrice = 150; //定数宣言のままだとエラー
+console.log(price * 140);
+console.log(price * 160);
 
-// console.log(150 * 140);
-// console.log(150 * 160);
-
-const price = 150;
-
+price = 170;
 console.log(price * 140);
 console.log(price * 160);
