45c6c95b7caeb3f56d6aedf6987c625911daebda
tetsunari <mtst.tty556@gmail.com>
2021-02-19 13:04:04 +0900

    addEventListener()

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -1,22 +1,18 @@
 'use strict';
 
-{
-  function update() {
-    // document.getElementById('target').textContent = 'Changed!';
-    // document.querySelector('p').textContent = 'Changed!';    //1番目のpタグ変わる
-    // document.querySelectorAll('p')[1].textContent = 'Changed!';  //２番目のpタグ変わる
-    document.querySelectorAll('p').forEach((p, index) => {
-      p.textContent = `${index}番目のpです！`;
-    });     //列ごとのpタグに作用する
-  }
+// { 
+//       document.querySelector('button').addEventListener('click', () => {
+//         document.getElementById('target').textContent = 'Changed!';
+//     });
+// }
 
-  setTimeout(update, 1000);
-}
-
-/** 
- * 重要
- * DOM で要素を取得する方法はこのようにいろいろありますが、 
- * id 属性が付いていたら getElementById() 、 
- * id 属性がなければ querySelector() か querySelectorAll() 
- * を使ってあげると良いでしょう。
+/**
+ * addEventListener()は第一引数にイベントの種類
  */
+    
+{
+    function update() {
+        document.getElementById('target').textContent = 'Change!';
+    }
+    document.querySelector('button').addEventListener('click', update);
+}
