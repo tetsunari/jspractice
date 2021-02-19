602416e8603e67269153caf4cfa2851162337d86
tetsunari <mtst.tty556@gmail.com>
2021-02-19 13:39:52 +0900

    HTMLのカスタムデータ属性をJavaScriptから扱う方法

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -1,19 +1,11 @@
 'use strict';
 
-{ 
+{
   document.querySelector('button').addEventListener('click', () => {
     const targetNode = document.getElementById('target');
 
-    // targetNode.className = 'my-color my-border';
-    // targetNode.classList.add('my-color');    //classList.add('my-color') としてあげれば、既存のクラスの設定に my-color を新しく追加してくれる
-
-
-    // if (targetNode.classList.contains('my-color') === true) {
-    //   targetNode.classList.remove('my-color');
-    // } else {
-    //   targetNode.classList.add('my-color');
-    // }
-    // -----------------------------------------------------------------------------
-    targetNode.classList.toggle('my-color');     // 上記のif文と同じ動作をする
+      // targetNode.textContent = 'Dotinstall';
+      targetNode.textContent = ttargetNode.data-translation;
+    //   targetNode.data-translation; では機能しない
   });
 }
