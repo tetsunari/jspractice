71f4d1b293bb7d7c777abf0251c74c3539816218
tetsunari <mtst.tty556@gmail.com>
2021-02-19 12:22:02 +0900

    複数スクリプト,スコープの分割

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -1,19 +1,6 @@
 'use strict';
 
-// const sum = function(a, b, c) {
-//   return a + b + c;
-// };
-// const sum = (a, b, c) => a + b + c;  //上と同じ
-
-
-// const total = sum(1, 2, 3) + sum(3, 4, 5);
-// console.log(total);
-
-
-
-
-// const double = function(a) {
-//   return a * 2;
-// };
-const double = a => a * 2;
-console.log(double(12));    //上と同じ
+{
+  const x = 100;
+  console.log(x);
+}
