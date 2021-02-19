53d236a3be3aab51ef7a67331720e529797e0d64
tetsunari <mtst.tty556@gmail.com>
2021-02-19 14:31:24 +0900

    チェックボックスで選択された値を使って要素を作成、DOMツリーに追加

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -3,16 +3,17 @@
 {
   document.querySelector('button').addEventListener('click', () => {
     const colors = document.querySelectorAll('input');
-    let selectedColor;
+    const selectedColors = [];
 
     colors.forEach(color => {
       if (color.checked === true) {
-        selectedColor = color.value;
+        selectedColors.push(color.value);
       }
     });
 
     const li = document.createElement('li');
-    li.textContent = selectedColor;
+    // li.textContent = selectedColors.join(',');   //で表現されるときに , 区切りになるので、 , 区切りの場合だったらこのように join() を書かなくても OK 
+    li.textContent = selectedColors;
     document.querySelector('ul').appendChild(li);
   });
 }
