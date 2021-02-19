8c6bd64feba867efa44b4a8c34084cfc1e748296
tetsunari <mtst.tty556@gmail.com>
2021-02-19 14:38:31 +0900

    ダブルクリックやマウス移動といった、クリック以外のイベント

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -1,19 +1,11 @@
 'use strict';
 
 {
-  document.querySelector('button').addEventListener('click', () => {
-    const colors = document.querySelectorAll('input');
-    const selectedColors = [];
+  document.querySelector('button').addEventListener('dblclick', () => {
+    console.log('Double Clicked!');
+  });
 
-    colors.forEach(color => {
-      if (color.checked === true) {
-        selectedColors.push(color.value);
-      }
-    });
-
-    const li = document.createElement('li');
-    // li.textContent = selectedColors.join(',');   //で表現されるときに , 区切りになるので、 , 区切りの場合だったらこのように join() を書かなくても OK 
-    li.textContent = selectedColors;
-    document.querySelector('ul').appendChild(li);
+  document.addEventListener('mousemove', () => {
+    console.log('moved!');
   });
 }
