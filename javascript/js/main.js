b2e246df0dbcbdfb896ac327362eee242701e9f6
tetsunari <mtst.tty556@gmail.com>
2021-02-19 13:38:22 +0900

    クラス属性を簡単に扱うためのclassList

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -4,7 +4,16 @@
   document.querySelector('button').addEventListener('click', () => {
     const targetNode = document.getElementById('target');
 
-    // targetNode.className = 'my-color';   //クリックするとボーダーが消える
-    targetNode.className = 'my-color my-border';
+    // targetNode.className = 'my-color my-border';
+    // targetNode.classList.add('my-color');    //classList.add('my-color') としてあげれば、既存のクラスの設定に my-color を新しく追加してくれる
+
+
+    // if (targetNode.classList.contains('my-color') === true) {
+    //   targetNode.classList.remove('my-color');
+    // } else {
+    //   targetNode.classList.add('my-color');
+    // }
+    // -----------------------------------------------------------------------------
+    targetNode.classList.toggle('my-color');     // 上記のif文と同じ動作をする
   });
 }
